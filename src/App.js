50895a50e5d0398fb65ba0552358989fe1d5e7d7
manugonzalezm <manuelgonzalez4a@gmail.com>
2021-08-30 02:05:00 -0300

    Armado de rutas en App con react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div>
-        
-    </div>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={ HeroListContainer }/>
+          <Route exact path='/:id' component={ HeroDetailContainer }/>
+          <Route exact path='/search-:name' component={ SearchListContainer }/>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
