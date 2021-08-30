dd2e3a91782a691006e5205b2a3ebb4dab623cdb
manugonzalezm <manuelgonzalez4a@gmail.com>
2021-08-30 20:31:19 -0300

    armado de mapeo en Item List

--- src/components/home_list/HeroList.js
+++ src/components/home_list/HeroList.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import HeroItem from '../HeroItem';
 
 //Lista de los superhéroes del equipo. 
 //Aquí obtenemos la información recibida de la API via props y la mapeamos con el componente "HeroItem".
-export default function HeroList() {
+export default function HeroList({ heroes, deleteHero }) {
     
     return (
-        <div>
-            < HeroItem
-                // key=...
-            />
-        </div>
+        <Row>
+            {heroes.map((item) => (
+                    <Col lg={4}>
+                        < HeroItem
+                            key={item.id}
+                            name={item.name}
+                            image={item.image.url}
+                            deleteHero={deleteHero}
+                            powerstats={item.powerstats}
+                        />
+                    </Col>
+                ))
+            }
+        </Row>
     )
 }
