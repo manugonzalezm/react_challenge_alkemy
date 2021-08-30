e21c825665289991b5e81345e2952a10d64c37b4
manugonzalezm <manuelgonzalez4a@gmail.com>
2021-08-30 16:48:12 -0300

    Creación de componente PowerstatsTotal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function Footer() {
-    return (
+    return ( 
         <div>
-            
+
         </div>
     )
 }
---
+++ src/components/home_list/PowerstatsTotal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//Este componente mostrará las powerstats totales del equipo asi como tambien la altura y peso promedio.
+export default function PowerstatsTotal() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
